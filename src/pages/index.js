259baadf4f12b12d7168a9edc1f34680500823cf
SpDmitryKalinin <spkalinin97@gmail.com>
2021-05-11 19:00:57 +0300

    Все блоки сверстаны

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,16 @@
-console.log('!!!');
+const swiper = new Swiper('.footer__slider', {
+    pagination:{
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    breakpoints:{
+        320: {
+            slidesPerView:1,
+        },
+        720:{
+            slidesPerView: 3,
+            spaceBetween: 30,
+        }
+    }
+});
+  
